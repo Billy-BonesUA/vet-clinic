00d72142eaa5d02f2857ac8b69fe12f2e3ff7c5a
Billy-BonesUA <zarinzomanu4@gmail.com>
2023-12-12 23:22:43 +0200

    REQ-02: Email, First name, Last name validation

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,7 @@
 
 =
 src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+T
+$src/Main/Java/com/magicvet/Main.java,9\c\9c43bded2c826c9036f169f8d3d6b024e0fc7bec
+T
+$src/main/java/com/magicvet/Main.java,4\9\49541b23f66cc7b7433ee2f093e2a95cb71d4cca
--- src/main/java/com/magicvet/Main.java
+++ src/main/java/com/magicvet/Main.java
@@ -1,17 +1,28 @@
 package main.java.com.magicvet;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static main.java.com.magicvet.Valid.*;
 
 public class Main {
 
     static String PASSWORD = "default";
     static Scanner SCANNER = new Scanner(System.in);
-
+    private static String EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    private static String FIRSTNAME_PATTERN = "^[a-zA-Z-]{3,}$";
+    private static String LASTNAME_PATTERN = "^[a-zA-Z-]{3,}$";
     public static void main(String[] args) {
         run();
     }
 
     static void run() {
+        if(auth()){
+            registerNewClient();
+        }
+    }
+    static boolean auth(){
         boolean accepted = false;
         for (int i = 0; i < 3; i++) {
             System.out.print("Password: ");
@@ -26,6 +37,59 @@ public class Main {
         }
 
         System.out.println(accepted ? "Welcome to the Magic Vet!" : "Application has been blocked.");
+        return accepted;
+    }
+    static void registerNewClient() {
+        System.out.println("Please provide client details.");
+
+        // Введення email
+        System.out.print("Email: ");
+        String email = SCANNER.nextLine();
+
+        // Перевірка валідності email
+        if (!Valid.isEmailValid(email)) {
+            System.out.println("Provided email is invalid.");
+            return;
+        }
+
+        // Створення об'єкта клієнта
+        Client client = new Client();
+        // Встановлення email клієнта
+        client.setEmail(email);
+
+        // Введення ім'я клієнта
+        System.out.print("First name: ");
+        String firstName = SCANNER.nextLine();
+
+        // Перевірка валідності імені
+        if (!Valid.isFirstNamelValid(firstName)) {
+            System.out.println("Provided first name is invalid.");
+            return;
+        }
+
+        // Введення прізвища клієнта
+        System.out.print("Last name: ");
+        String lastName = SCANNER.nextLine();
+
+        // Перевірка валідності прізвища
+        if (!Valid.isLastNamelValid(lastName)) {
+            System.out.println("Provided last name is invalid.");
+            return;
+        }
+
+        // Встановлення імені та прізвища клієнта
+        client.setFirstName(firstName);
+        client.setLastName(lastName);
+
+        // Виведення інформації про нового клієнта
+        System.out.println("New client: " + client.firstName + " " + client.lastName + " (" + client.email + ")");
     }
 
+
 }
+
+  /*  private static boolean isEmailValid(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return  matcher.matches();
+    }*/
